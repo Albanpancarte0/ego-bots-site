76b89c0cba8420e859724f6e6bafb934a1734a29
Albanpancarte0 <albanpancarte0@gmail.com>
2025-08-11 23:01:58 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
-module.exports = function () {
+module.exports = function(eleventyConfig) {
+  // Collection qui liste les pages "manager"
+  eleventyConfig.addCollection('teamManagers', (collection) => {
+    return collection.getAll().filter((item) =>
+      Array.isArray(item.data.tags) && item.data.tags.includes('team-manager')
+    );
+  });
   return {
     dir: { input: "src", output: "docs" },
     pathPrefix: "/ego-bots-site/"
